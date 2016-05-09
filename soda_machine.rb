51220a8f7d1e5eebf1c46c6fb3aa5caa3482dae6
Sneha Narasimhan <sbnarasimhan@gmail.com>
2016-05-09 14:58:35 -0700

    Checkpoint challenge, passes first test.

--- soda_machine.rb
+++ soda_machine.rb
@@ -1,18 +1,35 @@
+
 class SodaMachine
   attr_reader :sodas, :cash
+  attr_accessor :soda_count
+
+  @@soda_count = 0
 
   def initialize(args = {})
     @sodas = args[:sodas]
     @cash = args[:cash]
+    @@soda_count += @sodas.length
   end
 
   def current_inventory_count
+    @@soda_count
   end
 
   def find_soda(soda_brand)
+    @sodas.each do |brand|
+      if brand == soda_brand
+        return soda_brand
+      end
+      return soda_brand
+    end
   end
 
   def sell(soda_brand)
+    @sodas.delete_if do |brand|
+      if brand == soda_brand
+      return @cash
+      end
+    end    
   end
 
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,4 @@
 require_relative "../soda"
 require_relative "../soda_machine"
+
+# p soda_machine = SodaMachine.new(sodas: [pepsi, mountain_dew, coke_zero, second_pepsi], cash: 1.00)
